13ee032dc921979ee351fc325fbdcac76b0b67e3
camilo velasquez <camilovelasquez@gmail.com>
2025-03-08 16:04:36 -0500

    Corrección definitiva de error en Vercel

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "./", // 👈 Importante para evitar rutas incorrectas
+  build: {
+    outDir: "dist", // 👈 Asegura que Vercel use la carpeta correcta
+  }
 });
